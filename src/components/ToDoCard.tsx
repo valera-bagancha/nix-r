05fe173844a4c15b86147fa91ecc7382600daf34
valera-bagancha <valerabagancha@gmail.com>
2022-11-18 20:54:15 +0200

    final

--- src/components/ToDoCard.tsx
+++ src/components/ToDoCard.tsx
@@ -1,19 +1,12 @@
 
 import { useCallback, FC, useState, useEffect } from 'react'
-import { Card, CardActions, CardContent, Button, Typography } from '@mui/material';
+import { Card, CardActions, CardContent, Button, Typography, Box, MenuItem, InputLabel, FormControl, Select } from '@mui/material';
 import { STATUS } from '../types/enums'
-////////////////////////
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { useDispatch } from 'react-redux';
 import { deleteTodo, setEditableTodo, updateTodo } from '../redux/todos/actions';
 import { ITodo } from '../types';
 import { statusBgs } from '../constants/statusBgs';
-//////////////////////////////
+
 
 interface IProps {
   todo: ITodo;
